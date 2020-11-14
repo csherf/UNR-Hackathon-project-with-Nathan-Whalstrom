914d3f7b5f2c18b8b6a87236635a2351afbb3594
csherf <chadnsherf@gmail.com>
2020-11-14 14:05:50 -0800

    added resource bar

--- Player.gd
+++ Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody
 
 const UP = Vector3(0, 1.0, 0)
+const sprintSpeed = 2
 
-export var speed = 10.0
-export var look_speed = 0.5 / 1200.0
+export var speed = 1.7
+export var look_speed = 0.5 / 9000.0
+onready var sprintResource = $CanvasLayer/outline/background/resource_bar
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -14,6 +16,7 @@ func _input(event):
 
 func _physics_process(delta):
 	var move = Vector3.ZERO
+	var sprinting = false
 	if Input.is_action_pressed("move_forward"):
 		move += transform.basis.z
 	elif Input.is_action_pressed("move_backward"):
@@ -22,4 +25,10 @@ func _physics_process(delta):
 		move -= transform.basis.x
 	elif Input.is_action_pressed("move_left"):
 		move += transform.basis.x
+	if Input.is_action_pressed("sprint"):
+		sprinting = true
+	if Input.is_action_just_released("sprint"):	
+		sprinting = false
 	move_and_slide(move * speed, UP);
+	
+
