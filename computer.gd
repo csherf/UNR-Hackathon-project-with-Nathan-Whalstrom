cd1d55f45b09378e1374c107d727802635048744
Nathan Wahlstrom <--global>
2020-11-14 17:52:03 -0800

    next floor door setup

--- computer.gd
+++ computer.gd
@@ -19,3 +19,6 @@ func click(player):
 			player.task_text.text = "- Turn off computers: " + str(player.computers_left)
 		else:
 			player.task_text.text = "- Get to the next floor"
+			var next_level_door = Util.from_group("NextLevelDoor", self)
+			if next_level_door != null:
+				next_level_door.open()
