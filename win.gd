8966fcc10ce5444969a37198191b609049e6d5d7
csherf <chadnsherf@gmail.com>
2020-11-15 11:02:29 -0800

    sound

--- win.gd
+++ win.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
+	MusicController.play("res://tracks/elevator2.wav")
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://menu.tscn")
